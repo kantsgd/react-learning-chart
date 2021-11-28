a681e5361c1d83c1143a055cf74ff74df74cba09
Sait Goktug Dogan <dogan-sait@hotmail.com>
2021-11-29 00:21:31 +0300

    Now can select interval for the chart - only for second dropdown.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+let chartData = {
+  labels: ["January", "February", "March", "April", "May"],
+  datasets: [
+    {
+      label: "Number of Smart Phone Sales",
+      backgroundColor: "rgba(75,192,192,1)",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56],
+    },
+  ],
+};
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: chartData };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    let selectName = event.target.name;
+    let selectedValue = parseInt(event.target.value);
+
+    let updatedData = {...chartData};
+
+    if(selectName === "months2") {
+      updatedData.labels = updatedData.labels.slice(0, selectedValue+1);
+      updatedData.datasets[0].data.slice(0, selectedValue+1);
+
+      this.setState({data: updatedData});
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="wide" />
+        <div id="narrow">
+          <Bar
+            data={this.state.data}
+            options={{
+              title: {
+                display: true,
+                text: "Smart phone sale per month",
+                fontSize: 20,
+              },
+              legend: {
+                display: true,
+                position: "right",
+              },
+            }}
+          />
+          <label>Select Month interval </label>
+          <select name="months" onChange={this.handleChange}>
+            <option value="0">January</option>
+            <option value="1">February</option>
+            <option value="2">March</option>
+            <option value="3">April</option>
+            <option value="4">May</option>
+          </select>
+          <select name="months2" onChange={this.handleChange}>
+            <option value="0">January</option>
+            <option value="1">February</option>
+            <option value="2">March</option>
+            <option value="3">April</option>
+            <option value="4">May</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
